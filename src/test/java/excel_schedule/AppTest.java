891b66febc52a2ac984d08471cacb4060678e252
KingaBlaszczynska <kinga.blaszczynska@collibra.com>
2019-05-13 12:01:25 +0200

    Init

---
+++ src/test/java/excel_schedule/AppTest.java
@@ -0,0 +1,20 @@
+package excel_schedule;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
